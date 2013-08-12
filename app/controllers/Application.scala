5d34dfbf3a4a02f4bcb36720681d6debb8914ba6
Guillaume Bort <guillaume.bort@gmail.com>
2013-08-12 18:20:32 +0200

    first commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,53 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import scala.concurrent._
+import play.api.libs.concurrent.Execution.Implicits._
+
+import io.prismic._
+
+object Application extends Controller {
+
+  import Prismic._
+
+  // -- Resolve links to documents
+  def linkResolver(api: Api, ref: Option[String])(implicit request: RequestHeader) = DocumentLinkResolver(api) { 
+    case (Fragment.DocumentLink(id, docType, tags, slug, false), maybeBookmarked) => routes.Application.detail(id, slug, ref).absoluteURL()
+  }
+
+  // -- Page not found
+  def PageNotFound(implicit ctx: Prismic.Context) = NotFound("Page not found")
+
+  // -- Home page
+  def index(ref: Option[String]) = Prismic.action(ref) { implicit request =>
+    for {
+      someDocuments <- ctx.api.forms("everything").ref(ctx.ref).submit()
+    } yield {
+      Ok(views.html.index(someDocuments))
+    }
+  }
+
+  // -- Document detail
+  def detail(id: String, slug: String, ref: Option[String]) = Prismic.action(ref) { implicit request =>
+    for {
+      maybeDocument <- getDocument(id)
+    } yield {
+      checkSlug(maybeDocument, slug) {
+        case Left(newSlug) => MovedPermanently(routes.Application.detail(id, newSlug).url)
+        case Right(document) => Ok(views.html.detail(document))
+      }
+    } 
+  }
+
+  // -- Basic Search
+  def search(q: Option[String], ref: Option[String]) = Prismic.action(ref) { implicit request =>
+    for {
+      results <- ctx.api.forms("everything").query(s"""[[:d = fulltext(document, "${q.getOrElse("")}")]]""").ref(ctx.ref).submit()
+    } yield {
+      Ok(views.html.search(q, results))
+    }
+  }
+
+}
---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "prismicio-starter"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+
+    // Prismic.io Maven repository
+    resolvers += "Prismic.io kits" at "https://github.com/prismicio/repository/raw/master/maven/",
+    
+    // The Scala kit
+    libraryDependencies += "io.prismic" %% "scala-kit" % "1.0-SNAPSHOT"
+  )
+
+}
