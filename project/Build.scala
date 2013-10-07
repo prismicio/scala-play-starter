0a231ba8e0adcdae4b938220bbb1bacc8334bf25
Guillaume Bort <guillaume.bort@gmail.com>
2013-10-07 15:59:34 +0200

    Upgrade to Scala-Kit 1.0-M3

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object ApplicationBuild extends Build {
     resolvers += "Prismic.io kits" at "https://github.com/prismicio/repository/raw/master/maven/",
     
     // The Scala kit
-    libraryDependencies += "io.prismic" %% "scala-kit" % "1.0-M2"
+    libraryDependencies += "io.prismic" %% "scala-kit" % "1.0-M3"
   )
 
 }
