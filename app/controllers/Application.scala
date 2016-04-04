25e9b430c49940a5ba181af3495ab71bfca2d121
Erwan Loisant <eloisant@gmail.com>
2016-04-04 16:44:13 +0200

    Migrate to Play 2.5 and simplify the code by removing helpers

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
+import javax.inject._
+import play.api._
 import play.api.mvc._
+import play.api.Configuration
 
 import scala.concurrent._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -17,51 +20,85 @@ object Application {
 
 }
 
-class Application extends Controller with PrismicController {
+@Singleton
+class Application @Inject() (configuration: Configuration) extends Controller {
 
-  import PrismicHelper._
+  // -- Cache to use (default to keep 200 JSON responses in a LRU cache)
+  private val Cache = BuiltInCache(200)
 
+  // -- Write debug and error messages to the Play `prismic` logger (check the configuration in application.conf)
+  private val Logger = (level: Symbol, message: String) => level match {
+    case 'DEBUG => play.api.Logger("prismic").debug(message)
+    case 'ERROR => play.api.Logger("prismic").error(message)
+    case _      => play.api.Logger("prismic").info(message)
+  }
+
+  private def endpoint = configuration.getString("prismic.api").getOrElse(sys.error(s"Missing configuration prismic.api"))
+  private def token = configuration.getString("prismic.token")
+  private def fetchApi: Future[Api] = Api.get(endpoint, accessToken = token, cache = Cache, logger = Logger)
+
+  private def ref(api: Api)(implicit request: RequestHeader): String = {
+    val experimentRef: Option[String] = request.cookies.get(Prismic.experimentsCookie).map(_.value).flatMap(api.experiments.refFromCookie)
+    val previewRef: Option[String] = request.cookies.get(Prismic.previewCookie).map(_.value)
+    previewRef.orElse(experimentRef).getOrElse(api.master.ref)
+  }
 
   // -- Page not found
-  def PageNotFound(implicit ctx: PrismicHelper.Context) = NotFound(views.html.pageNotFound())
+  def PageNotFound = NotFound(views.html.pageNotFound())
 
-  def brokenLink = PrismicAction { implicit request =>
-    Future.successful(PageNotFound)
+  def brokenLink = Action { implicit request =>
+    PageNotFound
   }
 
   // -- Home page
-  def index(page: Int) = PrismicAction { implicit request =>
-    ctx.api.forms("everything").ref(ctx.ref).pageSize(10).page(page).submit() map { response =>
+  def index(page: Int) = Action.async { implicit request =>
+    for {
+      api <- fetchApi
+      response <- api.forms("everything").ref(ref(api)).pageSize(10).page(page).submit()
+    } yield {
       Ok(views.html.index(response))
     }
   }
 
   // -- Document detail
-  def detail(id: String, slug: String) = PrismicAction { implicit request =>
+  def detail(id: String, slug: String) = Action.async { implicit request =>
     for {
-      maybeDocument <- getDocument(id)
+      api <- fetchApi
+      maybeDocument <- api.forms("everything").ref(ref(api)).pageSize(1).submit().map(_.results.headOption)
     } yield {
       checkSlug(maybeDocument, slug) {
         case Left(newSlug)   => MovedPermanently(routes.Application.detail(id, newSlug).url)
-        case Right(document) => Ok(views.html.detail(document))
+        case Right(document) => Ok(views.html.detail(document, api))
       }
     }
   }
 
   // -- Basic Search
-  def search(q: Option[String], page: Int) = PrismicAction { implicit request =>
-    ctx.api.forms("everything")
-      .query(Predicate.fulltext("document", q.getOrElse("")))
-      .ref(ctx.ref).pageSize(10).page(page).submit() map { response =>
-        Ok(views.html.search(q, response))
-      }
+  def search(q: Option[String], page: Int) = Action.async { implicit request =>
+    for {
+      api <- fetchApi
+      response <- api.forms("everything").query(Predicate.fulltext("document", q.getOrElse(""))).ref(ref(api)).pageSize(10).page(page).submit()
+    } yield {
+      Ok(views.html.search(q, response, api))
+    }
   }
 
   // -- Preview Action
-  def preview(token: String) = PrismicAction { implicit req =>
-    ctx.api.previewSession(token, ctx.linkResolver, routes.Application.index().url).map { redirectUrl =>
+  def preview(token: String) = Action.async { implicit req =>
+    for {
+      api <- fetchApi
+      redirectUrl <- api.previewSession(token, Application.linkResolver(api), routes.Application.index().url)
+    } yield {
       Redirect(redirectUrl).withCookies(Cookie(Prismic.previewCookie, token, path = "/", maxAge = Some(30 * 60 * 1000), httpOnly = false))
     }
   }
 
+  // -- Helper: Check if the slug is valid and redirect to the most recent version id needed
+  def checkSlug(document: Option[Document], slug: String)(callback: Either[String, Document] => Result) =
+    document.collect {
+      case document if document.slug == slug         => callback(Right(document))
+      case document if document.slugs.contains(slug) => callback(Left(document.slug))
+    }.getOrElse {
+      Results.NotFound(views.html.pageNotFound())
+    }
 }
--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "prismicio-starter"
 
-version := "1.2"
+version := "2.0"
 
 scalaVersion := "2.11.7"
 
